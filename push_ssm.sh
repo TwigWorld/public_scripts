43b5fcd3c9bbdd411a21a86812a16556bfaace0a
Tom Bartlett <tbartlett@twig-world.com>
2018-05-21 16:26:48 +0100

    removed dead code

--- push_ssm.sh
+++ push_ssm.sh
@@ -9,6 +9,5 @@ for item in "${json_array[@]}"; do
     json_name="$(jq '.Name' <<< "$item")"
     json_value="$(jq '.Value' <<< "$item")"
     json_type="$(jq '.Type' <<< "$item")"
-#    aws ssm put-parameter --name "$json_name" --value "$json_value" --type "$json_type" --overwrite
     aws ssm put-parameter --cli-input-json "{ \"Name\": $json_name, \"Value\": $json_value, \"Type\": $json_type }" --overwrite
 done 
