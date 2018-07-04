dbad42225c1ed6c7cb9725481b5f154c126bceaa
Tom Bartlett <31888238+bartlettt@users.noreply.github.com>
2018-07-04 14:22:25 +0100

    Orphan detection (#12)
    * added orphan detection
    
    * helm list all
    
    * overwrite push

--- push_ssm.sh
+++ push_ssm.sh
@@ -9,5 +9,6 @@ for item in "${json_array[@]}"; do
     json_name="$(jq '.Name' <<< "$item")"
     json_value="$(jq '.Value' <<< "$item")"
     json_type="$(jq '.Type' <<< "$item")"
+    echo "'$item'"
     aws ssm put-parameter --cli-input-json "{ \"Name\": $json_name, \"Value\": $json_value, \"Type\": $json_type }" --overwrite
 done 
