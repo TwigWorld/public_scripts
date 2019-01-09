8a89471d96d0600a43c9577f93979978058e5306
Chris Wright <cjwright83y@gmail.com>
2019-01-09 15:19:29 +0000

    switched to using Rust tool to retrieve parameters from the AWS Parameter Store

--- getsetcheck_aws.sh
+++ getsetcheck_aws.sh
@@ -10,7 +10,7 @@ if [[ "${ENABLE_AWS_PARAMSTORE:-True}" =~ ^[Tt]rue$ ]]; then
   enable_aws_paramstore=true
 else
   enable_aws_paramstore=false
-fi 
+fi
 
 
 echo "\$0=$0"
@@ -25,21 +25,21 @@ check_set()
 {
     key="$1"
     value="$(eval echo \$$key)"
-    
+
     if [ -z "$value" ]; then
         echo "Error, environment variable '$key' not set!"
         exit 1
-    fi 
+    fi
 }
 
 pull_aws_params()
 {
-    json=$(aws --region eu-west-1 ssm get-parameters-by-path --path "$prefix" --with-decryption --output json)
+    json=$(ssm-get-parameters-by-path --path "$prefix")
     jq ".Parameters[]" <<< $json >> "${json_file}"
     next_token=$(jq --raw-output ".NextToken" <<< $json)
-    
+
     while [ "$next_token" != 'null' ]; do
-        json=$(aws --region eu-west-1 ssm get-parameters-by-path --path "$prefix" --with-decryption --output json --next-token $next_token)
+        json=$(ssm-get-parameters-by-path --path "$prefix" --next-token $next_token)
         jq ".Parameters[]" <<< $json >> "${json_file}"
         next_token=$(jq --raw-output ".NextToken" <<< $json)
     done
@@ -51,7 +51,7 @@ set_ssm_param()
     value="$(eval echo \$$key)"
     value_type=""
 
-    if [ -z "$value" ]; then 
+    if [ -z "$value" ]; then
 
         json_value="$(jq "select(.Name==\"${prefix}${key}\") | .Value" --raw-output <"${json_file}")"
         value_type="$(jq "select(.Name==\"${prefix}${key}\") | .Type" --raw-output <"${json_file}")"
@@ -59,7 +59,7 @@ set_ssm_param()
         if [ -z "$json_value" ]; then
             echo "Could not get value for '${prefix}${key}'"
             exit 1
-        fi 
+        fi
 
         value="$json_value"
     fi
@@ -70,7 +70,7 @@ set_ssm_param()
         echo "$key=$value"
     else
         echo "$key=<Redacted>"
-    fi 
+    fi
 }
 
 check_network()
@@ -92,7 +92,7 @@ check_network()
         if ((count >= limit)); then
             echo "Could not reach network resource '${host}:${port}'"
             exit 1
-        fi 
+        fi
         echo "Waiting for network resource at '${host}:${port}'"
         echo "Attempt ${count}/${limit}"
         sleep 1
@@ -102,27 +102,27 @@ check_network()
 }
 
 if [[ "$enable_aws_paramstore" == true ]]; then
-  for var in $mandatory_param_array; do 
+  for var in $mandatory_param_array; do
       check_set "$var"
       echo "$var='$(eval echo \$$var)'"
-  done 
+  done
   if [ ! -e "$json_file" ]; then
-      pull_aws_params 
-  fi 
-fi 
+      pull_aws_params
+  fi
+fi
 
 
 for var in $optional_param_array; do
-  if [[ "$enable_aws_paramstore" == true ]]; then 
+  if [[ "$enable_aws_paramstore" == true ]]; then
     set_ssm_param "$var"
   else
     check_set "$var"
-  fi 
-done 
+  fi
+done
 
-if [[ -z $skip_network_checks ]]; then 
+if [[ -z $skip_network_checks ]]; then
     for var in $network_socket_array; do
         echo "Checking network for '$var'"
         check_network "$var"
-    done 
-fi 
+    done
+fi
