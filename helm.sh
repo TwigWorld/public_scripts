906d82e3c2dce60c37624bf7674b266c0c3b6474
Tom Bartlett <31888238+bartlettt@users.noreply.github.com>
2019-03-11 16:05:43 +0000

    custom version (#18)

--- helm.sh
+++ helm.sh
@@ -2,13 +2,7 @@
 
 set -e
 
-helm_version="$(\
-  kubectl get pod --all-namespaces -l app=helm,name=tiller -o json | \
-  jq '.items[0].spec.containers[].image' --raw-output | \
-  head -n 1 | grep -Eo ':[^:]+$' | tr -d ':')"
-
-if [[ ! -x "${HOME}/.local/bin/helm-${helm_version}" ]]; then
-
+install_version() {
   echo "Installing helm-${helm_version}" >&2
 
   mkdir -p "/tmp/helm-${helm_version}"
@@ -20,6 +14,15 @@ if [[ ! -x "${HOME}/.local/bin/helm-${helm_version}" ]]; then
   tar --overwrite -C /tmp/helm-${helm_version} -xf /tmp/helm-${helm_version}/helm-${helm_version}-linux-amd64.tar.gz
   chmod +x /tmp/helm-${helm_version}/linux-amd64/helm
   mv /tmp/helm-${helm_version}/linux-amd64/helm "${HOME}/.local/bin/helm-${helm_version}"
+}
+
+helm_version="${HELM_VERSION:-$(\
+  kubectl get pod --all-namespaces -l app=helm,name=tiller -o json | \
+  jq '.items[0].spec.containers[].image' --raw-output | \
+  head -n 1 | grep -Eo ':[^:]+$' | tr -d ':')}"
+
+if [[ ! -x "${HOME}/.local/bin/helm-${helm_version}" ]]; then
+  install_version
 fi
 
 "${HOME}/.local/bin/helm-${helm_version}" $@
